48e6b16569e6dcb47955c6e778339607a003e34c
sudhanshu.joshi1 <sudhanshu.joshi1@DEL1-LHP-N00763>
2020-12-12 10:16:13 +0530

    Added a few exception handling for date-time.

--- Exercise/Opn/views.py
+++ Exercise/Opn/views.py
@@ -11,10 +11,19 @@ def BookTicket(request, id):
     movies = get_object_or_404(Movie, Movie_Id = id)
     if movies:
         name = movies.Name
-        day = request.POST['show_date']        
-        time = request.POST['show_time']
+        try:
+            day = request.POST['show_date']
+            time = request.POST['show_time']
+            datetime.datetime.strptime(day, '%Y-%m-%d')
+            datetime.datetime.strptime(time, '%H:%M')   
+        except:
+            return HttpResponseBadRequest("BAD REQUEST")
+        if datetime.datetime.strptime(day, '%Y-%m-%d') < datetime.datetime.now():
+            return HttpResponseBadRequest("You cannot select a previous date for booking tickets.")
+
         booking = BookingTable(movie_name=name, show_date=day, show_time=time)
         booking.save()
+        
     return HttpResponse('Booking Confirmed.')
 
 @require_http_methods(request_method_list=['DELETE'])
@@ -26,10 +35,20 @@ def CancelTicket(request, id):
 
 @require_http_methods(request_method_list=['POST'])
 def Reschedule(request, id):
-    booking = BookingTable.objects.get(Booking_Id = id)
-    if booking:
-        booking.show_date = request.POST.get('show_date')
-        booking.show_time = request.POST.get('show_time')
+    booking = get_object_or_404(BookingTable, Booking_Id = id)
+    try:
+        day = request.POST.get('show_date')
+        time = request.POST.get('show_time')
+        datetime.datetime.strptime(day, '%Y-%m-%d')
+        datetime.datetime.strptime(time, '%H:%M')
+    except:
+        return HttpResponseBadRequest("BAD REQUEST")
+
+    if datetime.datetime.strptime(day, '%Y-%m-%d') < datetime.datetime.now():
+            return HttpResponseBadRequest("You cannot select a previous date for booking tickets.") 
+
+    booking.show_date = day
+    booking.show_time = time
     booking.save()
     return HttpResponse('Booking Rescheduled.')
 
