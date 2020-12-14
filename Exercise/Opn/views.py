cdac29543952e12ed205a04b5e3c30c52c20b810
sudhanshu.joshi1 <sudhanshu.joshi1@DEL1-LHP-N00763>
2020-12-14 10:10:16 +0530

    Fixed bug for Reschedule

--- Exercise/Opn/views.py
+++ Exercise/Opn/views.py
@@ -39,16 +39,24 @@ def Reschedule(request, id):
     try:
         day = request.POST.get('show_date')
         time = request.POST.get('show_time')
-        datetime.datetime.strptime(day, '%Y-%m-%d')
-        datetime.datetime.strptime(time, '%H:%M')
+        
+        if day:
+            datetime.datetime.strptime(day, '%Y-%m-%d')
+        if time:
+            datetime.datetime.strptime(time, '%H:%M')
+        if not day and not time:
+            raise ValueError('Either change date or time.')        
+        
     except:
         return HttpResponseBadRequest("BAD REQUEST")
 
-    if datetime.datetime.strptime(day, '%Y-%m-%d') < datetime.datetime.now():
+    if day and datetime.datetime.strptime(day, '%Y-%m-%d') < datetime.datetime.now():
             return HttpResponseBadRequest("You cannot select a previous date for booking tickets.") 
 
-    booking.show_date = day
-    booking.show_time = time
+    if day:
+        booking.show_date = day
+    if time:
+        booking.show_time = time
     booking.save()
     return HttpResponse('Booking Rescheduled.')
 
